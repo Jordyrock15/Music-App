624942b295af2de2eafeb40c0058a7953fe26213
Jordan Barrand <Jordan.barrand@outlook.com>
2021-02-28 20:42:37 +0000

    First Half of App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
+//Adding components
+import Player from './components/Player';
+import Song from './components/Song';
+import Library from './components/Library';
+import Nav from './components/Nav';
+//Import Util
+import data from './util';
+//Import Styles
+import './styles/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//Ref
+	const audioRef = useRef(null);
+	//State
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duration: 0,
+	});
+	const [libraryStatus, setLibraryStatus] = useState(false);
+
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+		setSongInfo({ ...songInfo, currentTime: current, duration });
+	};
+	return (
+		<div className='App'>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+			<Song currentSong={currentSong} />
+			<Player
+				setSongInfo={setSongInfo}
+				songInfo={songInfo}
+				audioRef={audioRef}
+				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+				currentSong={currentSong}
+			/>
+			<Library
+				audioRef={audioRef}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+				setSongs={setSongs}
+				libraryStatus={libraryStatus}
+			/>
+			<audio
+				onLoadedMetadata={timeUpdateHandler}
+				onTimeUpdate={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+			></audio>
+		</div>
+	);
 }
 
 export default App;
